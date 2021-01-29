e689c59def8ee258262e0dfe120c693093e4a2ff
gram <mail@orsinium.dev>
2021-01-29 13:17:30 +0100

    parse opts

--- flake8_length/_checker.py
+++ flake8_length/_checker.py
@@ -1,78 +1,14 @@
 # built-in
-import enum
-import re
-import tokenize as tokens
 from ast import AST
 from tokenize import TokenInfo
 from typing import Iterator, Optional, Sequence, Tuple
 
+from flake8.options.manager import OptionManager
+
+from ._categories import Category, Categorizer
+
 STDIN = 'stdin'
 ViolationType = Tuple[int, int, str, type]
-
-
-class Category(enum.Enum):
-    EXCLUDED = enum.auto()
-    SOFT = enum.auto()
-    HARD = enum.auto()
-
-
-class Categorizer:
-    _excluded = frozenset({
-        tokens.NEWLINE,
-        tokens.ENCODING,
-        tokens.ENDMARKER,
-    })
-    _texts = frozenset({
-        tokens.STRING,
-        tokens.COMMENT,
-    })
-    _rex_url = re.compile(r'[a-z]://.+')
-    _rex_noqa = re.compile(r'(noqa|[nwer]:|pragma).+')
-    _rex_str = re.compile(r'[fbru](?:\'|"|\'\'\'|""")(.*)(?:\'|"|\'\'\'|""")')
-
-    def categorize(self, token: TokenInfo) -> Category:
-        if token.type in self._excluded:
-            return Category.EXCLUDED
-        content = self.get_content(token=token)
-        if content is None:
-            return Category.HARD
-
-        if token.type == tokens.COMMENT:
-            if self.is_url(content=content):
-                return Category.SOFT
-            if self.is_noqa(content=content):
-                return Category.SOFT
-
-        if token.type == tokens.STRING:
-            if len(content) < 5:
-                return Category.HARD
-            return Category.SOFT
-
-        return Category.HARD
-
-    def get_content(self, token: TokenInfo) -> Optional[str]:
-        if token.type not in self._texts:
-            return None
-        content = token.string
-        if token.type == tokens.COMMENT:
-            return content[1:].lstrip()
-
-        if token.type != tokens.STRING:
-            return content
-        match = self._rex_str.fullmatch(content)
-        if not match:
-            return content
-        return match.group(1)
-
-    def is_url(self, content: str) -> bool:
-        content = content.lower().strip()
-        match = self._rex_url.fullmatch(content)
-        return bool(match)
-
-    def is_noqa(self, content: str) -> bool:
-        content = content.lower().strip()
-        match = self._rex_noqa.fullmatch(content)
-        return bool(match)
 
 
 class Checker:
@@ -85,8 +21,28 @@ class Checker:
     _limit = 90
     _message = 'line is too long'
 
-    def __init__(self, tree: AST, file_tokens: Sequence[TokenInfo], filename: str = STDIN) -> None:
+    def __init__(
+        self,
+        tree: Optional[AST],
+        file_tokens: Sequence[TokenInfo],
+        filename: str = STDIN,
+    ) -> None:
         self._tokens = file_tokens
+
+    @classmethod
+    def add_options(cls, option_manager: OptionManager) -> None:
+        option_manager.add_option(
+            '--soft-line-length',
+            type='int',
+            metavar='n',
+            default=cls._limit,
+            parse_from_config=True,
+            help='Maximum allowed line length for non-special cases',
+        )
+
+    @classmethod
+    def parse_options(cls, options) -> None:
+        cls._limit = options.soft_line_length
 
     def run(self) -> Iterator[ViolationType]:
         for token in self._tokens:
